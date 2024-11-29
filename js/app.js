d18b6655060f81fa2a3338d630e70f83df4f5a54
JehuReyna <162845212+JehuReyna@users.noreply.github.com>
2024-11-29 15:07:42 -0600

    6

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ if ('serviceWorker' in navigator) {
     if (url.includes('localhost')) {
         swLocation = 'sw.js';  // Para desarrollo local
     } else {
-        swLocation = 'https://jehureyna.github.io/PWA-vitor-jehu/index.html';  // Ruta para GitHub Pages
+        swLocation = 'https://jehureyna.github.io/PWA-vitor-jehu/sw.js';  // Ruta para GitHub Pages
     }
 
     window.addEventListener('load', function() {
